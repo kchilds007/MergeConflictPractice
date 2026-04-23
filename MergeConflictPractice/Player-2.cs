adb29e92793221232af44e37ae787d88a39e1d41
HMoffett <hmoffett@chapman.edu>
2026-04-23 15:08:23 -0700

    Secret

--- MergeConflictPractice/Player-2.cs
+++ MergeConflictPractice/Player-2.cs
@@ -1,8 +1,8 @@
 ﻿public class Player
 {
     // TODO: branches will replace these 0 placeholders
-    public float MoveSpeed = 0f;
-    public float JumpForce = 0f;
+    public float MoveSpeed = 100000f;
+    public float JumpForce = 100000000f;
 
     public void Move(float horizontal)
     {
